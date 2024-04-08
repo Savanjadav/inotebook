cb91944581786982bd097511a1138bf24d43e309
SAVAN JADAV <savan.p.jadav@gmail.com>
2024-04-08 12:03:16 +0530

    Day3-3

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,6 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`iNotebook backend listening at https://localhost:${port}`)
 })
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,8 +2,20 @@ import { useState } from "react"
 import NoteContext from "./noteContext" 
 
 const NoteState = (props)=>{
+  const notesInitial=[
+    {
+      "_id": "6610e9f578696e6748112481",
+      "user": "660fe3cb0fe1648acb4b5927",
+      "title": "My title",
+      "description": "My first note",
+      "tag": "Personal",
+      "date": "2024-04-06T06:21:41.259Z",
+      "__v": 0
+    }
+  ]
+  const [notes, setNotes] = useState(notesInitial)
   return(
-    <NoteContext.Provider value={{}}>
+    <NoteContext.Provider value={{notes, setNotes}}>
       {props.children}
     </NoteContext.Provider>
   )
