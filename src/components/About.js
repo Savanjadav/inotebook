ef192e0abed7c5c9e0dd0b800dcac1d024e20a9a
SAVAN JADAV <savan.p.jadav@gmail.com>
2024-04-08 11:49:05 +0530

    Day3-2

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,9 @@ import { useContext } from 'react'
 import noteContext from '../context/notes/noteContext'
 
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(()=>{
-    a.update();
-    // eslint-disable-next-line
-  }, [])
   return (
     <div>
-      This is about {a.state.name} and he is in class {a.state.class}
+      This is about page
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,8 @@ import { useState } from "react"
 import NoteContext from "./noteContext" 
 
 const NoteState = (props)=>{
-  const s1={
-    "name":"Savan",
-    "class":"10-a"
-  }
-  const [state, setState] = useState(s1)
-  const update = () =>{
-    setTimeout(() => {
-      setState({
-        "name":"Yamee",
-        "class":"2-a"
-      })
-    }, 5000);
-  }
   return(
-    <NoteContext.Provider value={{state:state, update:update}}>
+    <NoteContext.Provider value={{}}>
       {props.children}
     </NoteContext.Provider>
   )
