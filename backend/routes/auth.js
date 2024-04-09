d1549b6957af0b7b7279d3540158e8fd1f8f7c60
SAVAN JADAV <savan.p.jadav@gmail.com>
2024-04-09 16:33:06 +0530

    Day4-4

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -55,6 +55,7 @@ router.post('/login', [
   body('email', 'Enter a valid email').isEmail(),
   body('password', 'Password can not be blank').exists()
 ], async (req, res) => {
+  let success = false;
   // If there are errors, return Bad request and the errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -65,11 +66,13 @@ router.post('/login', [
   try {
     let user = await User.findOne({ email });
     if (!user) {
+      success=false;
       return res.status(400).json({ error: "Please try to login with correct credentials" });
     }
     const passwordCompare = await bcrypt.compare(password, user.password);
     if (!passwordCompare) {
-      return res.status(400).json({ error: "Please try to login with correct credentials" });
+      success=false;
+      return res.status(400).json({success, error: "Please try to login with correct credentials" });
     }
     const data = {
       user: {
@@ -77,7 +80,8 @@ router.post('/login', [
       }
     }
     const authtoken = jwt.sign(data, JWT_SECRET);
-    res.json({ authtoken })
+    success= true;
+    res.json({ success,authtoken })
   } catch (error) {
     console.log(error.message);
     res.status(500).send("Intenal Server Error");
